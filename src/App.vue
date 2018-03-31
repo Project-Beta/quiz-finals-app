0e0a574011d1dc826515a21e5715e405b77c1651
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-31 11:56:34 +0530

    Add components of app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+
+	#app.flex.container
+
+		heading
+
+		//- router-view
+
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+
+	import heading from "./components/heading"
+
+	export default {
+		name: "App",
+		components: {
+			heading
+		}
+	}
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/styl/App.styl" lang="stylus"></style>
---
+++ src/components/heading.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+
+	h1.title
+		span.codeLight PROJECT
+		span.codeBold BETA
+		span.codeLight &nbsp;&nbsp;Quiz
+		span.codeBold Finals
+
+</template>
+
+<script>
+
+	export default {
+		name: "heading"
+	}
+
+</script>
+
+
+<style lang="stylus" scoped>
+
+	h1
+		color white
+		font-weight 100
+		text-align center
+		font-size 4vw
+		width 100%
+		padding 0.3em 0
+		border-bottom 1px solid #ccc
+
+		&:hover
+			cursor none
+
+	span
+
+		&.codeLight
+			font-family "Code Light"
+
+		&.codeBold
+			font-family "Code Bold"
+
+</style>
